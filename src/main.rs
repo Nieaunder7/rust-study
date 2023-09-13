c7bc6d071a8a588310e79931e80dd5a634ef443f
sieging <sieging@gmail.com>
2023-09-13 17:12:29 +0900

    변수 교환

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::io;
-
-fn fib(term: u32, val: u32, prev: u32) -> u32 {
-    if term == 0 {
-        return prev;
-    } else {
-        return fib(term - 1, val+prev , val);
+fn sum_array(input: &[i32]) -> i32 {
+    let mut sum = 0;
+    for i in input {
+        println!("{}", i);
+        sum = sum + i;
     }
+    sum
+}
+
+fn swap(lhs: &mut String, rhs: &mut String) {
+    std::mem::swap(lhs, rhs);
 }
 
 fn main() {
-    let mut guess = String::new();
-    let _ = io::stdin()
-        .read_line(&mut guess)
-        .expect("값을 읽는데 실패했습니다!");
-    let idx: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => return println!("0"),
-    };
-    println!("{}", fib(idx, 1, 0));
+    let input_arr = [1, 2, 3, 4, 5];
+    let sum_arr = sum_array(&input_arr);
+    const SUM_RESULT: i32 = 15;
+    assert!(sum_arr == SUM_RESULT);
+    println!("{sum_arr} == {SUM_RESULT}");
+
+    let mut lhs = String::from("AAA");
+    let mut rhs = String::from("BBB");
+    swap(&mut lhs, &mut rhs);
+    assert!(lhs == "BBB");
+    assert!(rhs == "AAA");
+    println!("{lhs} {rhs}");
 }
